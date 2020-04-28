49f36ad0d32af133d8b2016d3b944a794b02e419
gmar san <gmar@marsan.me>
2020-04-28 07:15:06 +0200

    remove passenger from dependencies list and installing it separately

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev postgresql libpq-dev nodejs imagemagick libicu-dev dirmngr gnupg apt-transport-https ca-certificates libnginx-mod-http-passenger"
+pkg_dependencies="autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev postgresql libpq-dev nodejs imagemagick libicu-dev dirmngr gnupg apt-transport-https ca-certificates"
 
 #=================================================
 # PERSONAL HELPERS
